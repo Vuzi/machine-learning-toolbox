e187755991d7f296c86a418b57fbfb6819f1d39e
Vuzi <vuzi@free.fr>
2016-04-27 00:26:43 +0200

    First implementations

--- MachineLearningToolbox/include/plugin.h
+++ MachineLearningToolbox/include/plugin.h
@@ -13,8 +13,21 @@
     #define EXPORT
 #endif
 
+// Tests
 EXPORT const char* getHelloWorld();
 EXPORT int add(int a, int b);
 EXPORT int mult(int a, int b);
+// End of tests
+
+// Exported methods
+EXPORT double* linearClassificationRosenblatt(double a, int yt, int y, double* x, double* w, unsigned n);
+EXPORT double* linearClassificationPLA(double a, int y, double* x, double* w, unsigned n);
+EXPORT int linearClassification(double* x, double* w, unsigned n);
+EXPORT void delPerceptron(double* w);
+EXPORT double* initPerceptron(unsigned n);
+
+// 'private' methods
+double randValue(double max, double min);
+int sign(double x);
 
 #endif //PLUGIN_H
--- MachineLearningToolbox/src/plugin.cpp
+++ MachineLearningToolbox/src/plugin.cpp
@@ -1,25 +1,117 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <time.h>
+
+#include <plugin.h>
 
 using namespace std;
 
-extern "C" {
-    const char* getHelloWorld() {
-        return "Hello world!";
-    }
+// Tests
+const char* getHelloWorld() {
+    return "Hello world!";
+}
 
-    int add(int a, int b) {
-        return a + b;
-    }
+int add(int a, int b) {
+    return a + b;
+}
 
-    int mult(int a, int b) {
-        return a * b;
-    }
+int mult(int a, int b) {
+    return a * b;
+}
+// End of tests
 
+/**
+ * Performs a Rosenblatt for the provided perceptron
+ * a  => learning step
+ * yt => observed result
+ * y  => expected result
+ * x* => input matrix
+ * w* => perceptron
+ */
+double* linearClassificationRosenblatt(double a, int yt, int y, double* x, double* w, unsigned n) {
+    for(unsigned i = 0; i < n; i++)
+        w[i] = w[i] + a * ((yt - y) * x[i]);
+
+    return w;
+}
+
+/**
+ * Performs a PLA for the provided perceptron
+ * a  => learning step
+ * y  => expected result
+ * x* => input matrix
+ * w* => perceptron
+ * n  => size
+ */
+double* linearClassificationPLA(double a, int y, double* x, double* w, unsigned n) {
+    for(unsigned i = 0; i < n; i++)
+        w[i] = w[i] + a * (y * x[i]);
+
+    return w;
+}
+
+/**
+ * Performs a linear classification on the provided perceptron with the provided input
+ * x* => input matrix
+ * w* => perceptron
+ * n  => size
+ */
+int linearClassification(double* x, double* w, unsigned n) {
+    double sum = 0;
+
+    for(unsigned i = 0; i < n; i++)
+        sum += w[i] * x[i];
+
+    return sign(sum);
+}
+
+/**
+ * Delete and free the provided perceptron
+ */
+void delPerceptron(double* w) {
+    delete [] w;
+}
+
+/**
+ * Init and return a perceptron of size n
+ * n  => size
+ */
+double* initPerceptron(unsigned n) {
+    double* m = new double[n];
+
+    for(unsigned i = 0; i < n; i++)
+        m[i] = randValue(1, -1);
+
+    return m;
+}
+
+/**
+ * Return a random double between the specified bounds
+ */
+double randValue(double max, double min) {
+    return (max - min) * ((double)rand() / (double)RAND_MAX) + min;
+}
+
+/**
+ * Return -1 if negative, otherwise 1
+ */
+int sign(double x) {
+    if(x < 0)
+        return -1;
+    else
+        return 1;
 }
 
 int main() {
     cout << "Hello, World!" << endl;
+
+    // TODO in some init method
+    srand((unsigned) time(NULL));
+
+    for (int n = 0; n < 10; ++n) {
+        std::cout << randValue(1, -1) << ' ';
+    }
+    std::cout << '\n';
 
     Eigen::MatrixXd m(2,2);
     m(0,0) = 3;
