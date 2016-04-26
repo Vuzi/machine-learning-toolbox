8327a172d7e7d8f43fd7b73401b4f8177224b69c
Vuzi <vuzi@free.fr>
2016-04-26 22:15:35 +0200

    Eigen support + CMake update

--- MachineLearningToolbox/src/plugin.cpp
+++ MachineLearningToolbox/src/plugin.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Eigen/Dense>
 
 using namespace std;
 
@@ -14,10 +15,18 @@ extern "C" {
     int mult(int a, int b) {
         return a * b;
     }
+
 }
 
 int main() {
     cout << "Hello, World!" << endl;
+
+    Eigen::MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
 
     return 0;
 }
